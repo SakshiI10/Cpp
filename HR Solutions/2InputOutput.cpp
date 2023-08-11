98502be908e596e3a211112da9956f4bfed8883f
Sakshi <iamsakshiingole@gmail.com>
2023-08-11 16:46:15 -0400

    HR Solution files added

---
+++ HR Solutions/2InputOutput.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int q,w,e,s;
+    cin >> q >> w >> e;
+    s=q+w+e;
+    cout << s;
+    return 0;
+}
