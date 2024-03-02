4c0c8e9516e64cbd1ae59c2c36d2348a4597b1e7
Sakshi <iamsakshiingole@gmail.com>
2024-03-02 11:33:19 -0500

    Added

---
+++ Data Structure and Algorithm/37_Permutation.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        do {
+            result.push_back(nums);
+        } while (next_permutation(nums.begin(), nums.end()));
+        return result;
+    }
+};
+
+int main() {
+    Solution solution;
+    // Example 1
+    vector<int> nums1 = {1, 2, 3};
+    vector<vector<int>> result1 = solution.permute(nums1);
+    cout << "Example 1:\n";
+    for (const auto& perm : result1) {
+        for (int num : perm) {
+            cout << num << ' ';
+        }
+        cout << '\n';
+    }
+    return 0;
+}
