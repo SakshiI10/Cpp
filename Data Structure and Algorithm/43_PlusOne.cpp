e8b6be1d27e6d436d2a3b32b8325d10ad7502574
Sakshi <iamsakshiingole@gmail.com>
2024-03-04 14:27:06 -0500

    File Added

---
+++ Data Structure and Algorithm/43_PlusOne.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        
+        // Start from the least significant digit
+        for (int i = n - 1; i >= 0; --i) {
+            // Increment the current digit
+            digits[i]++;
+            
+            // If there is no carry, return the result
+            if (digits[i] < 10) {
+                return digits;
+            } else {
+                // Carry over to the next digit
+                digits[i] %= 10;
+            }
+        }
+        
+        // If there is still a carry after the loop, insert a new digit at the beginning
+        digits.insert(digits.begin(), 1);
+        
+        return digits;
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> digits1 = {1, 2, 3};
+    vector<int> result1 = solution.plusOne(digits1);
+    cout << "Example 1 Result: [";
+    for (int digit : result1) {
+        cout << digit << " ";
+    }
+    cout << "]" << endl;
+    return 0;
+}
