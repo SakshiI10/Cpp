8bf8377ef95061b9f11b036952a32a8bd8cb78bb
Sakshi <iamsakshiingole@gmail.com>
2024-03-19 09:06:01 -0400

    File edited

--- Data Structure and Algorithm/17_PrintTheLeftEle.cpp
+++ Data Structure and Algorithm/17_PrintTheLeftEle.cpp
@@ -1,3 +1,8 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
 class Solution{
     public:
     int leftElement(int a[], int n) {
@@ -11,3 +16,13 @@ class Solution{
         return ele;
     }
 };
+
+int main(){
+    Solution sol;
+    int a[] = {7, 8, 3, 4, 2, 9, 5};
+    int n = sizeof(a)/sizeof(a[0]);
+    cout << "Output: ";
+    int k = sol.leftElement(a, n);
+    cout << k;
+    return 0;
+    }
