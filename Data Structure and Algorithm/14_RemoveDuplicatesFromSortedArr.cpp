bd2a1cb2b7a79c4a0b1c3bad698a8d621e702db8
Sakshi <iamsakshiingole@gmail.com>
2024-03-19 08:47:15 -0400

    File edited

--- Data Structure and Algorithm/14_RemoveDuplicatesFromSortedArr.cpp
+++ Data Structure and Algorithm/14_RemoveDuplicatesFromSortedArr.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+using namespace std;
 
-int removeDuplicates(std::vector<int>& nums) {
+int removeDuplicates(vector<int>& nums) {
     /* std::unique is an algorithm in the C++ Standard Template Library (STL) that eliminates consecutive duplicates in a range. nums.begin() and nums end() specify the range for the std::unique algorithm to operate on, which is the entire vector. std::unique rearranges the elements in the range such that all unique elements appear at the beginning, and it returns an iterator pointing to the end of the unique elements. */
-    auto it = std::unique(nums.begin(), nums.end());
+    auto it = unique(nums.begin(), nums.end());
     
     /* it is the iterator returned by std::unique, pointing to the end of the unique elements in the vector. erase is a vector method that removes elements in the specified range. This line effectively removes all elements from the iterator it to the end of the vector, leaving only the unique elements at the beginning. */
     nums.erase(it, nums.end());
@@ -14,15 +15,15 @@ int removeDuplicates(std::vector<int>& nums) {
 }
 
 int main() {
-    std::vector<int> nums = {1, 1, 2};
+    vector<int> nums = {1, 1, 2};
     int k = removeDuplicates(nums);
-    std::cout << "Output: " << k << ", nums = [";
+    cout << "Output: " << k << ", nums = [";
     for (int i = 0; i < k; ++i) {
-        std::cout << nums[i];
+        cout << nums[i];
         if (i < k - 1) {
-            std::cout << ",";
+            cout << ",";
         }
     }
-    std::cout << ", _]" << std::endl;
+    cout << ", _]" << endl;
     return 0;
 }
