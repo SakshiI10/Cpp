5fdc4b9482b2d713b330d0c8757a8700e36e0bc0
Sakshi <iamsakshiingole@gmail.com>
2024-03-19 08:58:04 -0400

    File edited

--- Data Structure and Algorithm/15_RemoveElement.cpp
+++ Data Structure and Algorithm/15_RemoveElement.cpp
@@ -1,15 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
 class Solution {
 public:
-    int removeElement(std::vector<int>& nums, int val) {
-        std::vector<int> SortedArr(nums);
-        sort(SortedArr.begin(), SortedArr.end());
-        for(int i = 0; i < SortedArr.size(); i++){
-            if(SortedArr[i] == val){
-                SortedArr.erase(SortedArr.begin() + i);
-                i--;  // Adjust index after erasing an element
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.size(); ++j) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
             }
         }
-        nums = SortedArr;  // Update the original nums vector
-        return nums.size();
+        return i;
     }
 };
+
+int main() {
+    Solution sol;
+    vector<int> nums = {3, 2, 2, 3};
+    int val = 2;
+    int k = sol.removeElement(nums, val);
+    cout << "Output: " << k << ", nums = [";
+    for (int i = 0; i < k; i++) {
+        cout << nums[i];
+        if (i < k - 1) {
+            cout << ",";
+        }
+    }
+    cout << "]" << endl;
+    return 0;
+}
