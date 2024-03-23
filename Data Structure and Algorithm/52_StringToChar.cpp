8b7a3458473eb9923826c474002804f98966bffd
Sakshi <iamsakshiingole@gmail.com>
2024-03-23 10:48:00 -0400

    File added

---
+++ Data Structure and Algorithm/52_StringToChar.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    vector<char> strtochar(string str) {
+        vector<char> charList;
+        for (char ch : str) {
+            charList.push_back(ch);
+        }
+        return charList;
+    }
+};
+
+int main() {
+    Solution solution;
+    string inputString;
+    cout << "Enter a string: ";
+    cin >> inputString;
+
+    vector<char> result = solution.strtochar(inputString);
+
+    cout << "Characters in the string: ";
+    for (char ch : result) {
+        cout << ch << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
