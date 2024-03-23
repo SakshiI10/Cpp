af30c58877ca74fea24b48af8939e951339c174d
Sakshi <iamsakshiingole@gmail.com>
2024-03-23 11:57:53 -0400

    File added

---
+++ Data Structure and Algorithm/59_Palindrome.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    string is_palindrome(int n) {
+        string numString = to_string(n);
+        string reversedString = numString;
+        reverse(reversedString.begin(), reversedString.end());
+        if (numString == reversedString) {
+            return "Yes";
+        } else {
+            return "No";
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+    int n;
+    
+    cout << "Enter an integer: ";
+    cin >> n;
+
+    string result = solution.is_palindrome(n);
+    
+    cout << "Is it a palindrome? " << result << endl;
+
+    return 0;
+}
