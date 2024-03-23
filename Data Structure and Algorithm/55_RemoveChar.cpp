36dad2984ce46bc86ca76fda0169f1873653c25f
Sakshi <iamsakshiingole@gmail.com>
2024-03-23 11:20:25 -0400

    File added

---
+++ Data Structure and Algorithm/55_RemoveChar.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype> // Include for isdigit function
+using namespace std;
+
+class Solution {
+public:	
+    string removeCharacters(string S) 
+    {
+        vector<char> charList;
+        for (char ch : S) {
+            charList.push_back(ch);
+        }
+        
+        string result;
+        for (char ch : charList) {
+            if (isdigit(ch)) {
+                result.push_back(ch);
+            }
+        }
+        return result;
+    }
+};
+
+/* 
+class Solution {
+public:	
+    vector<char> removeCharacters(string S) 
+    {
+        vector<char> charList;
+        for (char ch : S) {
+            charList.push_back(ch);
+        }
+        
+        vector<char> result;
+        for (char ch : charList) {
+            if (isdigit(ch)) {
+                result.push_back(ch);
+            }
+        }
+        return result;
+    }
+};
+ */
+
+int main() {
+    Solution solution;
+    string input;
+    
+    cout << "Enter a string: ";
+    cin >> input;
+
+    string result = solution.removeCharacters(input);
+    
+    cout << "Result after removing non-numeric characters: " << result << endl;
+
+    return 0;
+}
