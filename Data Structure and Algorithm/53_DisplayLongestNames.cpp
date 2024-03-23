28b758a61890fa5decc86e59a576260c0136e1d0
Sakshi <iamsakshiingole@gmail.com>
2024-03-23 10:53:36 -0400

    File added

---
+++ Data Structure and Algorithm/53_DisplayLongestNames.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string longest(int n, vector<string> &names) {
+        string longestName = names[0]; // Assuming the first name is the longest initially
+        for (int i = 1; i < n; ++i) {
+            if (names[i].size() > longestName.size()) {
+                longestName = names[i];
+            }
+        }
+        return longestName;
+    }
+};
+
+int main() {
+    Solution solution;
+    int n;
+    cout << "Enter the number of names: ";
+    cin >> n;
+
+    vector<string> names(n);
+    cout << "Enter " << n << " names: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> names[i];
+    }
+
+    string longestName = solution.longest(n, names);
+    cout << "Longest name: " << longestName << endl;
+
+    return 0;
+}
