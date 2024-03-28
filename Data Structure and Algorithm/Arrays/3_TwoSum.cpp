9aa7811c0d91b5db85c6f80f7cf731bcde4af1dc
Sakshi <iamsakshiingole@gmail.com>
2024-03-28 13:06:27 -0400

    File edited

--- Data Structure and Algorithm/Arrays/3_TwoSum.cpp
+++ Data Structure and Algorithm/Arrays/3_TwoSum.cpp
@@ -1,17 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map <int, int> m;
-        vector <int> v;
+        map<int, int> m;
+        vector<int> v;
         
         for (int i = 0; i < nums.size(); ++i) {
-        int complement = target - nums[i];
+            int complement = target - nums[i];
         
-        if (m.find(complement) != m.end()) {
-            return {m[complement], i};
+            if (m.find(complement) != m.end()) {
+                return {m[complement], i};
+            }
+            m[nums[i]] = i;
         }
-        m[nums[i]] = i;
+        return {}; // Return an empty vector if no solution is found
     }
-    return {}; // Return an empty vector if no solution is found
-}
 };
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    Solution sol;
+    vector<int> result = sol.twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices of the two numbers adding up to target: " << result[0] << ", " << result[1] << endl;
+
+        cout << "Numbers: " << nums[result[0]] << ", " << nums[result[1]] << endl;
+        
+    } else {
+        cout << "No solution found." << endl;
+    }
+    return 0;
+}
--- Data Structure and Algorithm/Arrays/4_MedianOf2SortedArrays.cpp
+++ Data Structure and Algorithm/Arrays/4_MedianOf2SortedArrays.cpp
@@ -1,23 +1,64 @@
-class Solution {
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
 public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        multiset <int> s;
-        for(int i=0; i<nums1.size(); i++)  {
-            s.insert(nums1[i]);
-        }
-        for(int i=0; i<nums2.size(); i++)  {
-            s.insert(nums2[i]);
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+    {
+        vector<int> merged;
+        int i = 0, j = 0;
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        while (i < n1 && j < n2)
+        {
+            if (nums1[i] < nums2[j])
+            {
+                merged.push_back(nums1[i]);
+                i++;
+            }
+            else
+            {
+                merged.push_back(nums2[j]);
+                j++;
+            }
         }
 
-        int totalSize = s.size();
-        multiset<int>::iterator mid = next(s.begin(), totalSize / 2);
-        
-        if (totalSize % 2 == 0) {
-            multiset<int>::iterator prevMid = prev(mid);
-            return static_cast<double>(*mid + *prevMid) / 2.0;
-        } 
-        else {
-            return static_cast<double>(*mid);
+        while (i < n1)
+        {
+            merged.push_back(nums1[i]);
+            i++;
+        }
+
+        while (j < n2)
+        {
+            merged.push_back(nums2[j]);
+            j++;
+        }
+
+        int totalSize = merged.size();
+        if (totalSize % 2 == 0)
+        {
+            return (merged[totalSize / 2 - 1] + merged[totalSize / 2]) / 2.0;
+        }
+        else
+        {
+            return merged[totalSize / 2];
         }
     }
 };
+
+int main()
+{
+    vector<int> nums1 = {1, 3};
+    vector<int> nums2 = {2};
+
+    Solution sol;
+    double median = sol.findMedianSortedArrays(nums1, nums2);
+
+    cout << "Median of the combined sorted arrays: " << median << endl;
+
+    return 0;
+}
