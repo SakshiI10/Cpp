028997e4ac44d6579929f901340fb4c16bcb5cdd
Sakshi <iamsakshiingole@gmail.com>
2024-03-28 12:29:30 -0400

    File edited

--- Data Structure and Algorithm/Arrays/2_PerfectArray.cpp
+++ Data Structure and Algorithm/Arrays/2_PerfectArray.cpp
@@ -1,12 +1,37 @@
-class Solution{
-    public:
-    bool IsPerfect(int a[],int n)
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    bool IsPerfect(int a[], int n)
     {
-        for(int i=0; i<n/2; i++){
-            if(a[i] != a[n-1-i]){
+        for (int i = 0; i < n / 2; i++)
+        {
+            if (a[i] != a[n - 1 - i])
+            {
                 return false;
             }
         }
         return true;
     }
 };
+
+int main()
+{
+    int a[] = {1, 2, 3, 1, 1};
+    int n = sizeof(a) / sizeof(a[0]);
+
+    Solution sol;
+    bool result = sol.IsPerfect(a, n);
+
+    if (result == 1)
+    {
+        cout << "It is a perfect Array" << endl;
+    }
+    else
+    {
+        cout << "It is not a perfect Array" << endl;
+    }
+}
