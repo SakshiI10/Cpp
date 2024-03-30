25fee0dd63cb1cfaf34a12176b83b8d64c3e1cba
Sakshi <iamsakshiingole@gmail.com>
2024-03-29 22:18:44 -0400

    File added

---
+++ Data Structure and Algorithm/Arrays/59_FindDuplicate.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Solution{
+public:
+    int findDuplicate(int A[]) {
+        if (A[3] == A[4])
+            return A[3];
+                
+        if (A[4] == A[5])
+            return A[4];
+                
+        return A[5];
+    }
+};
+
+int main() {
+    int A[] = {1, 2, 3, 3, 3, 3, 3, 5, 9, 10};
+    Solution solution;
+    int duplicate = solution.findDuplicate(A);
+    cout << "Duplicate number: " << duplicate << endl;
+
+    return 0;
+}
