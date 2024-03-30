7c33e31b7a356740cdfcdc1b0ae8bb1c5052897b
Sakshi <iamsakshiingole@gmail.com>
2024-03-30 13:39:37 -0400

    File added

---
+++ Data Structure and Algorithm/String/12_ToUpper.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+string to_upper(string str){
+    string result;
+    for(char ch: str){
+        result += toupper(ch);
+    }
+    return result;
+}
+
+int main() {
+    // Example 1
+    string str1 = "geeks";
+    cout << "Input: " << str1 << endl;
+    string result1 = to_upper(str1);
+    cout << "Output: " << result1 << endl;
+
+    return 0;
+}
