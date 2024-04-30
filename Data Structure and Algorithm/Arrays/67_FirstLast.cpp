f7140bb15fe442d8f7b281171269097e344d0f4e
Sakshi <iamsakshiingole@gmail.com>
2024-04-30 08:31:04 -0400

    File added

---
+++ Data Structure and Algorithm/Arrays/67_FirstLast.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> firstAndLast(vector<int> &arr, int n, int x)
+    {
+        vector<int> result{-1}; // Initialize result with -1 for cases where x is not found
+        int firstIndex = -1;
+
+        // Finding the first occurrence of x
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i] == x)
+            {
+                firstIndex = i;
+                break;
+            }
+        }
+
+        // If x is not found, return result with a single element as -1
+        if (firstIndex == -1)
+            return result;
+
+        // Finding the last occurrence of x
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (arr[i] == x)
+            {
+                result[0] = firstIndex; // Update first occurrence index
+                result.push_back(i);    // Add last occurrence index
+                break;
+            }
+        }
+        return result;
+    }
+};
+
+int main()
+{
+    Solution sol;
+
+    vector<int> arr = { 1, 3, 3, 4 };
+    int n = arr.size();
+    int x = 3; 
+
+    vector<int> indices = sol.firstAndLast(arr, n, x);
+    cout << "Indices of first and last occurrences of " << x << " are: ";
+    for (int i : indices)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
